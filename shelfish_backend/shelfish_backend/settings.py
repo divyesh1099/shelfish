c3d660ce2e8cde158e91cea99a319d250bb2f99c
Divyesh Vishwakarma <divyesh1099@gmail.com>
2023-05-06 00:49:05 +0530

    Configure Django REST Framework Backend

--- shelfish_backend/shelfish_backend/settings.py
+++ shelfish_backend/shelfish_backend/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -69,6 +71,10 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'shelfish_backend.wsgi.application'
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
